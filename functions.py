6d4a7d084a10c2daa39fa57b520d004a34f22899
Adrian Gonzalez Galvan <alu0101321219@ull.edu.es>
2022-02-24 10:03:49 +0000

    Comentarios varios en las funciones

--- functions.py
+++ functions.py
@@ -26,6 +26,8 @@ def isString(mystring):
 def stringToBinary(mystring):
 
     if (isString(mystring)):
+        # join: Unifica varias cadenas con el separador asignado, en este caso ''
+        # format: Cambia el formato a otro especificado, en nuestro caso de string a conjuntos de 8 bits
         return ''.join(format(ord(i), '08b') for i in mystring)
     else:
         return None
@@ -42,7 +44,7 @@ def isABinaryString(bstring):
     return True
 
 # Transforma una cadena binaria en un array de cadenas binarias de cierta longitud
-def stringToArrayOfStringS(mystring, long = 8):
+def stringToArrayOfStringS(mystring, long):
     
     myarray = []
     aux_string = ""
@@ -65,7 +67,7 @@ def binaryStringToASCIIString(bstring):
     ascii_string = ''
 
     if (isABinaryString(bstring)):
-        binary_array = stringToArrayOfStringS(bstring)
+        binary_array = stringToArrayOfStringS(bstring, 8)
         ascii_string = ''
         for element in binary_array:
             ascii_string += chr(int(element,2))
@@ -78,7 +80,10 @@ def binaryStringToASCIIString(bstring):
 def bStringXORBString(bstring1, bstring2):
 
     if (isABinaryString(bstring1) and isABinaryString(bstring2)):
+        # Operador XOR en Python: ^ (opera con 2 int)
+        # int(string, base): convierte un string en un entero de base 2(binaria)
         numeric_result = int(bstring1, 2) ^ int(bstring2, 2)
+        # {0:b} = formato binario
         binary_result = '{0:b}'.format(numeric_result)
         while (len(binary_result) != len(bstring1) or len(binary_result) != len(bstring2)):
             binary_result = '0' + binary_result
